45bb9a255008351bac23658dd456087209704d74
ppapahr <philpapachristou@gmail.com>
2020-03-22 14:59:47 +0200

    Correct merge of CommandFactory versions

--- Text2Speech/src/command/CommandFactory.java
+++ Text2Speech/src/command/CommandFactory.java
@@ -1,40 +1,40 @@
 package command;
 
 import java.awt.event.ActionListener;
-import javax.swing.*;
 import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class CommandFactory {
-	
+
 	JTextArea textArea;
 	JFrame frame;
 	public ActionListener newListener = null;
-	
+
 	private NewDocument newDocument;
-	private OpenDocument openDocument;
 	private SaveDocument saveDocument;
-	
-	public CommandFactory(JTextArea textArea) {
+	private OpenDocument openDocument;
+
+	public CommandFactory(JTextArea textArea, JFrame frame) {
 		//GUI Components
 		this.textArea = textArea;
 		this.frame = frame;
-		
+
 		//initialize commands
 		newDocument = new NewDocument(textArea);
-		openDocument = new OpenDocument(textArea, frame);
 		saveDocument = new SaveDocument(textArea);
-		
+		openDocument = new OpenDocument(textArea, frame);
+
 	}
 	
 	public ActionListener createCommand(String s) {
 		ActionListener temp = null;
 		if(s.equals("New")) {
 			return newDocument;
+		} else if(s.equals("Save")) {
+			return saveDocument;
 		}
 		else if(s.equals("Open...")) {
 			return openDocument;
-		} else if(s.equals("Save")) {
-			return saveDocument;
 		}
 		//add other commands
 		return temp;
