31c3cf355ca387ef54d625a3c75876f4ef81d295
Grigoris Tsopouridis <43859151+gtsopus@users.noreply.github.com>
2020-04-20 16:00:53 +0300

    freettsadapter

--- Text2Speech/src/text2speechapis/FreeTTSAdapter.java
+++ Text2Speech/src/text2speechapis/FreeTTSAdapter.java
@@ -1,5 +1,39 @@
 package text2speechapis;
 
-public class FreeTTSAdapter {
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
 
+//ALL FREETTS COPYRIGHTS BELONG TO THEIR RESPECTIVE OWNERS
+//freetts.sourceforge.io
+
+public class FreeTTSAdapter implements TextToSpeechAPI{
+	
+	private Voice voice;
+	private VoiceManager vm;
+	
+	public FreeTTSAdapter() {
+		//set a voice and initialize it
+		this.voice = VoiceManager.getInstance().getVoice("kevin16");
+		this.voice.allocate();
+	}
+
+	@Override
+	public void play(String text) {
+		voice.speak(text);
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		voice.setVolume(volume);
+	}
+
+	@Override
+	public void setRate(int rate) {
+		voice.setRate(rate);
+	}
+
+	@Override
+	public void setPitch(int pitch) {
+		voice.setPitch(pitch);
+	}
 }
