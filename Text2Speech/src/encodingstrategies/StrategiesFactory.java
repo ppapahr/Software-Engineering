fe03d8a7a84affaa87f1d78a12d71ae10fa11b0c
Grigoris Tsopouridis <43859151+gtsopus@users.noreply.github.com>
2020-05-06 18:09:27 +0300

    Update StrategiesFactory.java

--- Text2Speech/src/encodingstrategies/StrategiesFactory.java
+++ Text2Speech/src/encodingstrategies/StrategiesFactory.java
@@ -2,11 +2,19 @@ package encodingstrategies;
 
 public class StrategiesFactory {
 	
-	public StrategiesFactory() {
-		
-	}
+	private EncodingStrategy dummyEncodingStrategy;
+
 	
+	public StrategiesFactory() {}
+	
+	//instantiates and returns an encoding strategy depending on a string
 	public EncodingStrategy createStrategy(String s) {
-		return null;
+		if(s == "rot13" || s == "Rot13") {
+			dummyEncodingStrategy = new Rot13Encoding();
+		}
+		else if(s == "atbash" || s == "atBash"|| s == "Atbash"|| s == "AtBash") {
+			dummyEncodingStrategy = new AtBashEncoding();
+		}
+		return dummyEncodingStrategy;
 	}
 }
