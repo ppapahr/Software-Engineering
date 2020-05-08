b39e0a363fcccd66d140565d6f4014446bd5d0e8
GiannisTar <johnnytar98@gmail.com>
2020-05-08 17:41:00 +0300

    Rot13 and atbash encoding

--- Text2Speech/src/encodingstrategies/AtBashEncoding.java
+++ Text2Speech/src/encodingstrategies/AtBashEncoding.java
@@ -5,11 +5,39 @@ public class AtBashEncoding extends TemplateEncoding{
 	public AtBashEncoding() {
 		
 	}
+	
+	public String encode(String s) {
+		char[] charArray = s.toCharArray();
+		for(int i=0; i<charArray.length; i++) {
+			charArray[i] = this.mapCharacter(charArray[i]);
+		}
+		String encodedS = new String(charArray);
+		return encodedS;
+	}
 
 
 	@Override
 	public char mapCharacter(char c) {
-		// TODO Auto-generated method stub
+		//if char is a letter we subtract from the last letter the ascii number of c and add the first letter to return to the original range
+		if(Character.isLetter(c)) {
+			if(Character.isUpperCase(c)) {
+				int ascii = (int) c ;
+				int rotInt = 'Z' - ascii + 'A';
+				char rotChar = (char) rotInt;
+				return rotChar;
+			}
+			else if(Character.isLowerCase(c)) {
+				int ascii = (int) c ;
+				int rotInt = 'z' - ascii + 'a';
+				char rotChar = (char) rotInt;
+				return rotChar;
+			}
+		}
+		//if c is not a letter then return it
+		else {
+			return c;
+		}
+		System.out.println("Something's wrong");
 		return 0;
 	}
 
--- Text2Speech/src/encodingstrategies/Rot13Encoding.java
+++ Text2Speech/src/encodingstrategies/Rot13Encoding.java
@@ -6,10 +6,48 @@ public class Rot13Encoding extends TemplateEncoding{
 		
 	}
 
+	public String encode(String s) {
+		char[] charArray = s.toCharArray();
+		for(int i=0; i<charArray.length; i++) {
+			charArray[i] = this.mapCharacter(charArray[i]);
+		}
+		String encodedS = new String(charArray);
+		return encodedS;
+	}
 
 	@Override
 	public char mapCharacter(char c) {
-		// TODO Auto-generated method stub
+		if(Character.isLetter(c)) {
+			if(Character.isUpperCase(c)) {
+				if(c < 'M') {
+					c += 13;
+					return c;
+				}
+				else {
+					//if c+13 > 'Z' then we subtract the difference from 'A' plus 12 digits for Rot encoding
+					int rotInt =  'A' + 12 - ('Z' - c);
+					char rotChar = (char) rotInt;
+					return rotChar;
+				}
+			}
+			else if(Character.isLowerCase(c)) {
+				if(c < 'm') {
+					c += 13;
+					return c;
+				}
+				else {
+					//if c+13 > 'z' then we subtract the difference from 'a' plus 12 digits for Rot encoding
+					int rotInt =  'a' + 12 - ('z' - c);
+					char rotChar = (char) rotInt;
+					return rotChar;
+				}
+			}
+		}
+		else {
+			//if c is not a letter then return it
+			return c;
+		}
+		System.out.println("Something's wrong");
 		return 0;
 	}
 
