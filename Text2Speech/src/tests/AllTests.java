c0f791ac3a1a3b6f648e0e4d329d166dcbd955cd
Grigoris Tsopouridis <43859151+gtsopus@users.noreply.github.com>
2020-05-22 19:53:45 +0300

    alltests updated

--- Text2Speech/src/tests/AllTests.java
+++ Text2Speech/src/tests/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ EditDocumentTest.class, NewDocumentTest.class, OpenDocumentTest.class, SaveDocumentTest.class,
-		SpeechEncodingTest.class, TuneAudioTest.class })
+		SpeechEncodingTest.class, TuneAudioTest.class,TuneEncodingTest.class })
 public class AllTests {
 
 }
